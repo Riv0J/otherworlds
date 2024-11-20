76eed98b779e81027ccd8cb697fce0bb6a591d8b
Rivo <107114580+Riv0J@users.noreply.github.com>
2024-11-20 10:37:42 +0100

    Update place_editor.blade.php

--- resources/views/modules/place_editor.blade.php
+++ resources/views/modules/place_editor.blade.php
@@ -1,4 +1,4 @@
-<script src='{{ asset('modules/Place_Editor.js') }}?v=3'></script>
+<script src='{{ asset('modules/Place_Editor.js') }}?v=4'></script>
 <script>
     function show_place_editor(place){
         const editor_data = {
