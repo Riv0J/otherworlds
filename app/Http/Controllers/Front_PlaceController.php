ae136708b405897e6e2d0d72750dec31f54d93af
Rivo <107114580+Riv0J@users.noreply.github.com>
2024-11-26 22:28:46 +0100

    Update Front_PlaceController.php

--- app/Http/Controllers/Front_PlaceController.php
+++ app/Http/Controllers/Front_PlaceController.php
@@ -116,7 +116,7 @@ class Front_PlaceController extends Controller{
         //         ->get('places.*');
 
         // get the places ordered by favorites
-        $places = Place::orderBy('id', 'asc')
+        $places = Place::orderBy('views_count', 'desc')
             ->skip($startIndex)
             ->take(Front_PlaceController::PER_PAGE)
             ->get('places.*');
